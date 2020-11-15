4db66a7634a0be78f0f0b07af8aa23905a688a5b
Heegyu Kim <heekue83@gmail.com>
2020-11-15 22:07:00 +0900

    blacked

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -3,18 +3,19 @@ from torch.utils.data import DataLoader
 from hkspell.dataset import SpellDataset
 
 
-
 @pytest.fixture
 def dataset():
-    return SpellDataset({
-        "aple": ["able", "apple", "apply"],
-        "correctd": ["corrects", "correct"],
-        "helpp": ["help", "helped"]
-    })
+    return SpellDataset(
+        {
+            "aple": ["able", "apple", "apply"],
+            "correctd": ["corrects", "correct"],
+            "helpp": ["help", "helped"],
+        }
+    )
 
 
 def test_dataset_with_loader(dataset):
-    
+
     loader = DataLoader(dataset, 2)
 
     X, y = next(iter(loader))
