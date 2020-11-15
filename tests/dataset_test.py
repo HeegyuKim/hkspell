63f4103bea2141ac130dbd7e4629ea1eec3c867f
Heegyu Kim <heekue83@gmail.com>
2020-11-15 22:05:10 +0900

    Revert "fixup: Format Python code with Black"
    This reverts commit f25ab474071359eb1f36b3660109dcbb1dbc7a33.

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -3,19 +3,18 @@ from torch.utils.data import DataLoader
 from hkspell.dataset import SpellDataset
 
 
+
 @pytest.fixture
 def dataset():
-    return SpellDataset(
-        {
-            "aple": ["able", "apple", "apply"],
-            "correctd": ["corrects", "correct"],
-            "helpp": ["help", "helped"],
-        }
-    )
+    return SpellDataset({
+        "aple": ["able", "apple", "apply"],
+        "correctd": ["corrects", "correct"],
+        "helpp": ["help", "helped"]
+    })
 
 
 def test_dataset_with_loader(dataset):
-
+    
     loader = DataLoader(dataset, 2)
 
     X, y = next(iter(loader))
