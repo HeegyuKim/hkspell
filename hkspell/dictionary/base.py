efa30abd3c2463f0a8e28cfbc1639f11263c0dec
Heegyu Kim <heekue83@gmail.com>
2020-11-18 00:29:41 +0900

    PeterNorvigSpellChecker 구현 추가

---
+++ hkspell/dictionary/base.py
@@ -0,0 +1,18 @@
+
+class BaseDictionary:
+    
+    def __init__(self) -> None:
+        pass
+    
+    def __len__(self):
+        return 0
+
+    def __contains__(self, word: str):
+        return False
+
+    def probability_of(self, word: str) -> float:
+        """
+            주어진 단어의 등장확률을 반환합니다
+            단어가 없을 시 None
+        """
+        pass
