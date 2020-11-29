a0ff48af557b72750be8e42dbad0a6844529e846
Heegyu Kim <heekue83@gmail.com>
2020-11-29 18:37:22 +0900

    PeterNorvig, SymSpell 구현체 추가 (#7)
    * PeterNorvigSpellChecker 구현 추가
    
    * symspell is implemented
    
    * remote black in requirements.txt
    
    * test code에서 주석 제거

---
+++ tests/checker/test_symspell.py
@@ -0,0 +1,51 @@
+import pytest
+import os
+from hkspell.checker.symspell import from_english_text, SymSpellChecker
+
+
+@pytest.fixture
+def checker():
+
+    text = """
+    Yesterday
+    All my troubles seemed so far away
+    Now it looks as though they're here to stay
+    Oh, I believe in yesterday
+    Suddenly
+    I'm not half the man I used to be
+    There's a shadow hangin' over me
+    Oh, yesterday came suddenly
+    Why she had to go, I don't know, she wouldn't say
+    I said something wrong, now I long for yesterday
+    Yesterday
+    Love was such an easy game to play
+    Now I need a place to hide away
+    Oh, I believe in yesterday
+    Why she had to go, I don't
+    """
+
+    return from_english_text(text)
+
+
+def test_corrections(checker: SymSpellChecker):
+
+    words = ["beleive", "sometghin"]
+    corrections = ["believe", "something"]
+
+    for word, correct in zip(words, corrections):
+        corrects = checker.corrections(word, 10, return_probabilities=True)
+
+        assert len(corrects) <= 10
+        assert corrects[0][0] == correct
+
+
+def test_words_contains(checker: SymSpellChecker):
+
+    words_in = ["yesterday", "far", "play"]
+    words_not_in = ["happy", "fruit", "computer"]
+
+    for word in words_in:
+        assert word in checker.dictionary
+
+    for word in words_not_in:
+        assert word not in checker.dictionary
